a965c6bf711cd2c6c8b275369dcf2a2c4a92f5ca
Aditi Goyal <102579070+aditi5050@users.noreply.github.com>
2023-02-11 12:54:05 +0530

    first commit

---
+++ src/components/MostSold.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function MostSold() {
+  return (
+    <div className="most-sold-container">
+      <div className="container">
+        <div className="title-container">
+          <h2>Most Solid </h2>
+          <p>
+            This is the section about the data which product with most sold in our store.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MostSold;
